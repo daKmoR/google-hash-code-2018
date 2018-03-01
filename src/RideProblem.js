f919c607c2ed9d88afb39d454ebd6ec20eb2cb2e
Thomas Allmer <thomas.allmer@ing.nl>
2018-03-01 20:41:43 +0000

    fix: allow test to run

--- src/RideProblem.js
+++ src/RideProblem.js
@@ -68,7 +68,7 @@ class RideProblem {
             if(car.ride.startedAt === car.ride.startLimit) {
               this.score += this.startBonus;
             }
-            
+
             car.completedRides.push(car.ride.rideId);
             car.ride = null;
           } else {
@@ -131,6 +131,6 @@ class RideProblem {
   }
 }
 
-if(module) {
+if (typeof module === 'object') {
   module.exports = RideProblem;
 }
