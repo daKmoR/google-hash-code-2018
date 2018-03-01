fb5ed88e61603a6146af681a39a6b532dcdb2c5d
Thomas Allmer <thomas.allmer@ing.nl>
2018-03-01 19:34:42 +0000

    fix: invalid code

--- src/RideSolution.js
+++ src/RideSolution.js
@@ -3,15 +3,15 @@ class RideSolution {
     const inputRows = input.split('\n').filter(Boolean);
 
     this.cars = inputRows.slice(0).map((carRow) => {
-      return { rides: carRow.split(' ').splice(1).map(Number) }; 
+      return { rides: carRow.split(' ').splice(1).map(Number) };
     });
   }
 
   createBruteForceSolution(input) {
     return [...Array(input.vehicles).keys()]
   }
-}
 
-RideSolution.toString() = function() {
-  let output = '';
+  toString() {
+    let output = '';
+  }
 }
