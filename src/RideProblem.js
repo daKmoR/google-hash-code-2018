fc572869d2ed33f5ebfab9b24e6ff89f6cd804f7
Jimi van der Woning <Jimi.van.der.Woning@ing.nl>
2018-03-01 19:15:07 +0100

    feat: add problem parser

---
+++ src/RideProblem.js
@@ -0,0 +1,18 @@
+class RideProblem {
+  constructor(input) {
+    const inputRows = input.split('\n').filter(Boolean);
+
+    const [rows, cols, vehicles, rideCount, startBonus, stepLimit] = inputRows[0].split(' ');
+    this.rows = +rows;
+    this.cols = +cols;
+    this.vehicles = +vehicles;
+    this.rideCount = +rideCount;
+    this.startBonus = +startBonus;
+    this.stepLimit = +stepLimit;
+
+    this.rides = inputRows.slice(1).map((rideRow) => {
+      const [startRow, startCol, endRow, endCol, startLimit, endLimit] = rideRow.split(' ');
+      return { startRow: +startRow, startCol: +startCol, endRow: +endRow, endCol: +endCol, startLimit: +startLimit, endLimit: +endLimit, };
+    });
+  }
+}
---
+++ test/RideProblemSpec.js
@@ -0,0 +1,26 @@
+describe('Ride problem', () => {
+
+  it('should extract the correct raw data', function() {
+    const problem = new RideProblem(`3 4 2 3 2 10
+0 0 1 3 2 9
+1 2 1 0 0 9
+2 0 2 2 0 9
+`);
+
+    expect(problem.rows).to.equal(3);
+    expect(problem.cols).to.equal(4);
+    expect(problem.vehicles).to.equal(2);
+    expect(problem.rideCount).to.equal(3);
+    expect(problem.startBonus).to.equal(2);
+    expect(problem.stepLimit).to.equal(10);
+    expect(problem.rides.length).to.equal(3);
+    
+    expect(problem.rides[0].startRow).to.equal(0);
+    expect(problem.rides[0].startCol).to.equal(0);
+    expect(problem.rides[0].endRow).to.equal(1);
+    expect(problem.rides[0].endCol).to.equal(3);
+    expect(problem.rides[0].startLimit).to.equal(2);
+    expect(problem.rides[0].endLimit).to.equal(9);
+  });
+
+});
